c4389dbeced984f6bc4d1eaa098754bcc5bcfe86
AndiniRodia16 <44723909+AndiniRodia16@users.noreply.github.com>
2022-11-27 21:34:00 +0700

    Add files via upload

---
+++ MODUL4 DINI/wad_modul4_dini.sql
@@ -0,0 +1,58 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 27 Nov 2022 pada 15.22
+-- Versi server: 10.4.27-MariaDB
+-- Versi PHP: 8.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `wad_modul4_dini`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `showroom_dini_table`
+--
+
+CREATE TABLE `showroom_dini_table` (
+  `id_mobil` int(255) NOT NULL,
+  `nama_mobil` varchar(255) NOT NULL,
+  `pemilik_mobil` varchar(255) NOT NULL,
+  `merk_mobil` varchar(255) NOT NULL,
+  `tanggal_beli` date NOT NULL,
+  `deskripsi` text NOT NULL,
+  `foto_mobil` varchar(255) NOT NULL,
+  `status_pembayaran` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user_dini`
+--
+
+CREATE TABLE `user_dini` (
+  `id` bigint(20) NOT NULL,
+  `email` int(255) NOT NULL,
+  `nama` int(255) NOT NULL,
+  `no_hp` int(50) NOT NULL,
+  `password` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
